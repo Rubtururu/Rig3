a286fb69ec67e4476d60a59a9c70b6681c3bdcbd
Rubtururu <59855904+Rubtururu@users.noreply.github.com>
2024-06-17 01:03:06 +0200

    Update eth_magic.js

--- js/eth_magic.js
+++ js/eth_magic.js
@@ -15,7 +15,7 @@ window.addEventListener('load', function () {
 });
 // WEB3 INIT DONE!
 
-const contract_address = "0x5b3DED94BC66FA895FCe0A95F609e5433d6CC2B5";
+const contract_address = "0x0425B0DA87f9d609624B4874B47E341B3c441019";
 var account = web3.eth.accounts[0];
 
 function startApp(web3) {
